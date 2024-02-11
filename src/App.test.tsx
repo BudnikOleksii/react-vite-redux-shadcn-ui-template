0571e791aede841f683353eb6ac8b447dee09a34
Oleksii Budnik <budnikoleksii@gmail.com>
2024-02-11 20:21:39 +0200

    chore: set up and configure shadcn ui

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
+import { renderWithProviders } from '@/shared/utils/test-utils';
 import { screen, waitFor } from '@testing-library/react';
 
 import App from './App';
-import { renderWithProviders } from './utils/test-utils';
 
 test('App should have correct initial render', () => {
   renderWithProviders(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+import * as path from 'path';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'react-vite-redux-tw-template',
+  base: '/react-vite-redux-shadcn-ui-template/', // TODO document to set up this
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     open: true,
   },
